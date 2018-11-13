c89f2a5d7975cb28a012922ffde81d60d43b2148
pc2-PC\pc2 <cesarmencia7@gmail.com>
2018-11-13 11:37:00 -0600

    no message

---
+++ saludar/saludar/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace saludar
+{
+    public partial class 
+         
+         
+        
+         Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.SelectAll();
+            if (textBox1.SelectionLength>0 )
+            {
+                
+                
+                    MessageBox.Show(("Hola  " + textBox1.Text), "Saludo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                
+            }
+            else
+            {
+                MessageBox.Show("introdusca un texto", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
